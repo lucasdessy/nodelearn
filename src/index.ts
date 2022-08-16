d6e7ecb2d4f118848e268525052c19a68033c4eb
GitJooJ <jpavicari@hotmail.com>
2022-08-15 23:23:47 -0300

    feat: rough API functionality test with insomnia

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,33 @@
-import express, { response } from 'express';
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello World Ignite!" });
-})
+app.use(express.json());
+
+app.get("/courses", (req, res) => {
+  const query = req.query;
+  console.log(query);
+  return res.json(["Curso 1", "Curso 2", "Curso 3"]);
+});
+
+app.post("/courses", (req, res) => {
+  const body = req.body;
+  console.log(body);
+  return res.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);  
+});
+
+app.put("/courses/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(id)
+  return res.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.patch("/courses/:id", (req, res) => {	
+  return res.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
+});
+
+app.delete("/courses/:id", (req, res) => {
+  return res.json(["Curso 6", "Curso 7", "Curso 4"]);
+});
 
 app.listen(3333);
