53c18b9783763fa9578ad83ab5411ac0e4d480d7
GitJooJ <jpavicari@hotmail.com>
2022-08-19 16:32:06 -0300

    feat: created method get statement
    updated readme and created the statement method for cpf consulting

--- README.md
+++ README.md
@@ -4,8 +4,8 @@
 
 ### Requisitos
 
-- [] Deve ser possível criar umma conta
-- [] Deve ser possível buscar o extrato bancário do cliente
+- [X] Deve ser possível criar uma conta
+- [X] Deve ser possível buscar o extrato bancário do cliente
 - [] Deve ser possível realizar um depósito
 - [] Deve ser possível realizar um saque
 - [] Deve ser possível buscar o extrato bancário do cliente por data
@@ -17,9 +17,9 @@
 
 ### Regras de negócio
 
-- [] Não deve ser possível cadastrar uma conta com CPF já existente
+- [X] Não deve ser possível cadastrar uma conta com CPF já existente
+- [X] Não deve ser possível buscar extrato em uma conta não existente
 - [] Não deve ser possível fazer depósito em uma conta não existente
-- [] Não deve ser possível buscar extrato em uma conta não existente
 - [] Não deve ser possível fazer saque em uma conta não existente
 - [] Não deve ser possível excluir uma conta não existente
 - [] Não deve ser possível fazer saque quando o saldo for insuficiente
--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,40 @@ import { v4 as uuidv4 } from 'uuid';
 
 const app = express();
 
-const costumers = [];
+const customers = [];
 
 app.use(express.json());
 
 app.post("/account", (req, res) => {
-    const { cpf, name } = req.body;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
-    const id = uuidv4();
-    costumers.push({ 
-        id, 
+    const { cpf, name } = req.body; 
+    
+    const customerAlreadyExists = customers.find(
+        costumers => customers.some(
+            (customer) => customer.cpf === cpf
+        ));
+        
+        if (customerAlreadyExists) {
+            return res.status(400).json({ error: "Costumer already exists" });
+        }
+
+    customers.push({ 
         cpf, 
         name, 
+        id: uuidv4(),
         statement: []
     });
     return res.status(201).send();
 });
 
+app.get("/statement", (req, res) => {
+    const { cpf } = req.headers;
+
+    const customer = customers.find(
+        customer => customer.cpf === cpf );
+    
+    if (!customer) {
+        return res.status(400).json({ error: "Costumer not found" });
+    }
+    return res.json(customer.statement);
+});
 app.listen(3333);
