595f8908a480fef12a58724388e5b5200bfb63da
GitJooJ <jpavicari@hotmail.com>
2022-08-19 14:22:52 -0300

    feat: account creation with cpf
    added http method to account creation with unique cpf and uuid

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 const app = express();
+
+const costumers = [];
+
+app.use(express.json());
+
+app.post("/account", (req, res) => {
+    const { cpf, name } = req.body;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+    const id = uuidv4();
+    costumers.push({ 
+        id, 
+        cpf, 
+        name, 
+        statement: []
+    });
+    return res.status(201).send();
+});
 
 app.listen(3333);
