ab304d90b5ae4cb3ad6a6c6167fe28b3769d1384
GitJooJ <jpavicari@hotmail.com>
2022-08-22 10:31:57 -0300

    feat: added middlewares for method requisitions
    feature is still not functional, needs polishing

---
+++ src/@types/express.d.ts
@@ -0,0 +1,8 @@
+declare namespace Express {
+  export interface Request {
+    customer: {
+      id: string;
+      cpf: string;
+    };
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,21 @@ const customers = [];
 
 app.use(express.json());
 
+//middlewares
+function verifyIfExistsAccountCPF(req, res, next) {
+  const { cpf } = req.headers;
+
+  const customer = customers.find(
+    customer => customer.cpf === cpf );
+    
+  if (!customer) {
+    return res.status(400).json({ error: "Costumer not found" });
+  }
+
+  req.customer = customer;
+  return next();
+}
+
 app.post("/account", (req, res) => {
     const { cpf, name } = req.body; 
     
@@ -25,18 +40,14 @@ app.post("/account", (req, res) => {
         id: uuidv4(),
         statement: []
     });
+
+
+
     return res.status(201).send();
 });
 
-app.get("/statement", (req, res) => {
-    const { cpf } = req.headers;
-
-    const customer = customers.find(
-        customer => customer.cpf === cpf );
-    
-    if (!customer) {
-        return res.status(400).json({ error: "Costumer not found" });
-    }
-    return res.json(customer.statement);
+app.get("/statement", verifyIfExistsAccountCPF, (req, res) => { 
+  const { customer } = req;
+  return res.json(customer.statement);
 });
 app.listen(3333);
